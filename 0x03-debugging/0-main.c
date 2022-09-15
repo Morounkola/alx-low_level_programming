3cd9b17ae35e69863ae1a912b56cc3293bab9aed
Morounkola <samuelsolanke2000@gmail.com>
2022-09-14 22:40:34 -0700

    Task O solution

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Entry point
+ *
+ * Description: Test positive_or_negative() function
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	//i = 98;
+	positive_or_negative(i);
+
+	return(0);
+}
