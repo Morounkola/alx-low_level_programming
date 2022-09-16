50c657f01634a32a56e6aad9e674c6725b7e317a
Morounkola <samuelsolanke2000@gmail.com>
2022-09-16 16:18:21 -0700

    Task 2 soln

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mult - main entry
+ * @a: first number to be multiplied with b
+ * @b: second number to be multiplied with a
+ * Description: determines the product of two numbers
+ * Return: The product of the two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
