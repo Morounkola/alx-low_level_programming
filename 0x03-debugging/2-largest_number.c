f4ea9da07f4eed0b0ebd35459776f2e98f72477a
Morounkola <samuelsolanke2000@gmail.com>
2022-09-16 05:35:36 -0700

    Task 2 submission

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,20 +2,16 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * 
  * @a: first integer
- *
  * @b: second integer
- *
  * @c: third integer
- *
  * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -28,8 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
-
-
