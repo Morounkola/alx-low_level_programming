d82ce4a4d7ddc4c2b4b321bf58bc05b07c7c0c45
Morounkola <samuelsolanke2000@gmail.com>
2022-09-16 17:00:03 -0700

    Task 4 soln

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Task 4 main entry
+ * Description: print numbers from 0 to 9, excluding 2 & 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
