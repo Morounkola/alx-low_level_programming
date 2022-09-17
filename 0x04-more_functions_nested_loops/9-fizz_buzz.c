d241e13e1c1da812c0907a260af8b7e9fdc60329
Morounkola <samuelsolanke2000@gmail.com>
2022-09-16 20:32:00 -0700

    Task 9 soln

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,34 +10,34 @@
 
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	i = 1;
-	while (i < 101)
+	while (i <= 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else
 		{
-			printf("%d", i);
-			printf(" ");
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
 		}
 
 		i++;
 	}
-	printf("\n");
+	putchar('\n');
+	return (0);
 }
 
