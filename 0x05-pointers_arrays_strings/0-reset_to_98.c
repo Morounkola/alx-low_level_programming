0fb3d1ed6e02cbfe68567210c7ff0d4d1815f468
Morounkola <samuelsolanke2000@gmail.com>
2022-09-20 08:29:50 -0700

    Task-0 Soln

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Task O code
+ * Description: takes a pointer to a variable and change it value
+ * @n: value to be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
