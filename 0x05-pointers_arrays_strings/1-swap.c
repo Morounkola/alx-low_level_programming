fa055abc37bfb96bc424f985369352d0688c57c8
Morounkola <samuelsolanke2000@gmail.com>
2022-09-20 15:04:25 -0700

    Task 1 soln: swap int values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Task 1 code
+ * Description: write a function that swaps the values of two integar
+ * @a: first integer, to be swapped with the second
+ * @b: second integer, to be swapped with the first
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*b = *a;
+	*b = c;
+}
