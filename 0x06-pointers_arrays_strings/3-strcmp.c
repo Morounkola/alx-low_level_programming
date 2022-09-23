f36c6f707ae60ee598038828641cbf6e7b90bb07
Morounkola <samuelsolanke2000@gmail.com>
2022-09-23 13:57:16 -0700

    Task 3 Soln: compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _stcmp - main entry for Task 1
+ * _strcmp - main entry for Task 3
  * Description: function that compares 2 strings
  * @s1: first string, to be compared with the second string
  * @s2: second string, to be compared with the first string
  * Return: concatenated string
  */
 
-int *_strcmp(char *s, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int cnt, dif;
 
