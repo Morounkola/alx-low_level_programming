6581f52b2f597c7bf14550e6c5230c26bc8dd06f
Morounkola <samuelsolanke2000@gmail.com>
2022-09-23 13:06:53 -0700

    Task 0 Soln: String concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,15 +3,15 @@
 /**
  * _strcat - main entry for Task 0
  * Description: function to join 2 strings together
- * @*dest: first string, to which the second will be added to
- * @*src: second string that willbe added to the *dest
+ * @dest: first string, to which the second will be added to
+ * @src: second string that willbe added to the *dest
  * Return: concatenated string
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int len, x;
-	
+
 	len = 0;
 	while (dest[len] != '\0')
 	{
