303d1202d01b1196d6059a60dd2b97583e2bcc4c
Morounkola <samuelsolanke2000@gmail.com>
2022-10-11 13:49:17 -0700

    Task soln

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to free space
+ * @d: pointer to dog typedef type
+ * Return: always success
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
